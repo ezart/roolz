2a2376aec9fc02557feb9a2ed4037d3e813d7142
ezart <ezart193@gmail.com>
2018-02-17 13:35:57 +0300

    Fixed less than rool

--- lib/rool/less_than.rb
+++ lib/rool/less_than.rb
@@ -1,5 +1,10 @@
-# Inverse of greater than
+# Checks whether the @operand is less than the value in the dataset
 module Rool
   class LessThan < Basic
+  	def process(dataset)
+  		super
+  		return false unless (dataset[@datakey].respond_to?(:<) && @operand.respond_to?(:<))
+  		@operand < dataset[@datakey]
+  	end
   end
 end
