fd48173a4fe4bdd0d99df54d0a3b09c230951add
ezart <ezart193@gmail.com>
2018-03-18 10:06:28 +0300

    Fix the not part of not rule

--- lib/rool/not.rb
+++ lib/rool/not.rb
@@ -2,5 +2,9 @@
 # It returns the boolean inverse of the result of its child rule.
 module Rool
   class Not < Container
+  	def process(dataset={})	
+  		raise ArgumentError.new("Expected only one child rule as an argument") unless @children.length == 1
+  		!@children[0].process(dataset)
+  	end
   end
 end
